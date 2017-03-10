cad9791c543a1423e028fe98a933f240ff0b37de
Vernon <me@example.com>
2017-03-10 10:42:38 -0600

    beautiful code

--- assets/scripts/controllers/main.js
+++ assets/scripts/controllers/main.js
@@ -1,37 +1,24 @@
 'use strict';
 
 angular.module('chattyApp')
-  .controller('MainCtrl', function ( $scope, messageService ) {
+    .controller('MainCtrl', function($scope, messageService) {
 
+        messageService.getMessages().then(function(response) {
 
-
-    messageService.getMessages().then(function ( response ) {
-
-      $scope.messages = response.data;
-    });
-
-    $scope.addMessage = function ( obj ) {
-
-      console.log("before if");
-      if (obj) {
-
-        messageService.addMessage(obj).then(function ( response ) {
-          console.log(response);
-          $scope.messages = response.data;
-
-
+            $scope.messages = response.data;
         });
-      }
-
-
-
-    };
-
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
-
-  });
+        $scope.addMessage = function(obj) {
+            console.log("before if");
+            if (obj) {
+                messageService.addMessage(obj).then(function(response) {
+                    console.log(response);
+                    $scope.messages = response.data;
+                });
+            }
+        };
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
